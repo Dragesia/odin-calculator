eafe09f5b6cbaeeb1a9e25ff072351c702883e5e
Dragesia <brkgzlsy@gmail.com>
2022-07-07 22:41:24 +0300

    selected all buttons

---
+++ script.js
@@ -0,0 +1,23 @@
+// selecting all buttons
+const text = document.querySelector(".text");
+const allButtons = document.querySelectorAll(".btn");
+const sum = document.querySelector(".sum");
+const sub = document.querySelector(".sub");
+const mul = document.querySelector(".mul");
+const dvs = document.querySelector(".dvs");
+const dot = document.querySelector(".dot");
+const clear = document.querySelector(".clear");
+const enter = document.querySelector(".enter");
+const zero = document.querySelector(".zero");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+
+
+// event listeners 
