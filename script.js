003b05117bd092df8e84c079814d484b75fe432d
Dragesia <brkgzlsy@gmail.com>
2022-07-08 18:13:57 +0300

    refresh start

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ const nine = document.querySelector(".nine");
 
 let textValue = "";
 let values = [];
-let currentMode = "free";
-let lastMode;
 
 // event listeners
 
@@ -38,82 +36,3 @@ allButtons.forEach(btn => {
     btn.onmouseup = () => btn.classList.remove("btn-click");
 })
 
-allNumbers.forEach(num => {
-    num.onclick = writeFromScreen;
-});
-
-sum.onclick = () => changeMode("+");
-sub.onclick = () => changeMode("-");
-mul.onclick = () => changeMode("x");
-dvs.onclick = () => changeMode("/");
-
-clear.onclick = () => {
-    clearText();
-    changeMode("free");
-}
-
-deleteBtn.onclick = deleteText;
-
-//functions
-
-function clearText() {
-    values = [];
-    textValue = ""
-    lastText.innerHTML = textValue;
-    currentText.innerHTML = textValue;
-}
-
-function deleteText () {
-    textValue = textValue.slice(0, -1);
-    currentText.innerHTML = textValue;
-}
-
-function writeFromScreen(e) {
-    if (textValue == "") {
-        if (e.target.innerHTML == ".") return;
-    }
-    if (textValue.includes(".") && e.target.innerHTML == ".") return;
-    if (textValue == "0" && e.target.innerHTML == "0") return;
-    textValue += e.target.innerHTML;
-    currentText.innerHTML = textValue;
-}
-
-function changeMode(newMode) {
-    if (currentMode == "free" || newMode == "free") {
-        currentMode = "free";
-        sum.classList.remove("btn-on");
-        sub.classList.remove("btn-on");
-        mul.classList.remove("btn-on");
-        dvs.classList.remove("btn-on");
-    } if (currentMode == newMode) {
-        currentMode = "free";
-        sum.classList.remove("btn-on");
-        sub.classList.remove("btn-on");
-        mul.classList.remove("btn-on");
-        dvs.classList.remove("btn-on");
-    } else if (newMode == "+") {
-        currentMode = "+";
-        sum.classList.add("btn-on");
-        sub.classList.remove("btn-on");
-        mul.classList.remove("btn-on");
-        dvs.classList.remove("btn-on");
-    } else if (newMode == "-") {
-        currentMode = "-";
-        sum.classList.remove("btn-on");
-        sub.classList.add("btn-on");
-        mul.classList.remove("btn-on");
-        dvs.classList.remove("btn-on");
-    } else if (newMode == "x") {
-        currentMode = "x";
-        sum.classList.remove("btn-on");
-        sub.classList.remove("btn-on");
-        mul.classList.add("btn-on");
-        dvs.classList.remove("btn-on");
-    } else if (newMode == "/") {
-        currentMode = "/";
-        sum.classList.remove("btn-on");
-        sub.classList.remove("btn-on");
-        mul.classList.remove("btn-on");
-        dvs.classList.add("btn-on");
-    }
-}
